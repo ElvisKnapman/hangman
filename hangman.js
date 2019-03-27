c2abcf5a0c91d706717236b6617b845750479566
Elvis Knapman <elvis@elvisknapman.com>
2019-03-27 19:23:30 -0400

    Add getPuzzle function to Hangman prototype to display current state of puzzle (hidden and guessed letters)

--- hangman.js
+++ hangman.js
@@ -1,8 +1,26 @@
 'use strict';
 
-function Hangman(word, guesses) {
+function Hangman(word, remainingGuesses) {
     this.word = word.toLowerCase().split('');
-    this.guesses = guesses;
+    this.remainingGuesses = remainingGuesses;
     this.guessedLetters = [];
 
 }
+
+
+Hangman.prototype.getPuzzle = function(guess) {
+    let puzzle = '';
+    // if current letter is in guessedLetters array, or is a space, reveal in output...
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter;
+        // if not, place an asterisk in place of the letter to hide it in output
+        } else {
+            puzzle += '*';
+        }
+    });
+
+    return puzzle;
+}
+
+const game1 = new Hangman('Cat', 2);
