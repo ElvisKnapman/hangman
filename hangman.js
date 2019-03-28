a234bd3b5ee69257492229eac370b588aae00b57
Elvis Knapman <elvis@elvisknapman.com>
2019-03-28 13:12:47 -0400

    Create makeGuess function and add to Hangman prototype. Attach event listener to window to listen for key presses

--- hangman.js
+++ hangman.js
@@ -7,8 +7,7 @@ function Hangman(word, remainingGuesses) {
 
 }
 
-
-Hangman.prototype.getPuzzle = function(guess) {
+Hangman.prototype.getPuzzle = function() {
     let puzzle = '';
     // if current letter is in guessedLetters array, or is a space, reveal in output...
     this.word.forEach((letter) => {
@@ -23,4 +22,31 @@ Hangman.prototype.getPuzzle = function(guess) {
     return puzzle;
 }
 
+Hangman.prototype.makeGuess = function(guess) {
+    // convert guess to be case insensitive
+    guess = guess.toLowerCase();
+    const isUniqueGuess = !this.guessedLetters.includes(guess);
+    const isBadGuess = !this.word.includes(guess);
+
+    // check if guessed letter is unique, if not, add letter to array
+    if (isUniqueGuess) {
+        this.guessedLetters.push(guess);
+    }
+
+        // if word does not contain the guessed letter, subtract 1 guess
+    if(isUniqueGuess && isBadGuess) {
+            this.remainingGuesses--;
+    } 
+}
+
+
 const game1 = new Hangman('Cat', 2);
+console.log(game1.getPuzzle());
+console.log(game1.remainingGuesses);
+
+window.addEventListener('keypress', event => {
+    const guess = String.fromCharCode(event.charCode);
+    game1.makeGuess(guess);
+    console.log(game1.getPuzzle());
+    console.log(game1.remainingGuesses);
+});
